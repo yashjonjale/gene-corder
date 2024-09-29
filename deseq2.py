7ac085ebcbe1d84999aab943438713db2a3b474f
Saket Choudhary <saketkc@gmail.com>
2024-09-30 02:26:56 +0530

    Add deseq2 template

--- deseq2.py
+++ deseq2.py
@@ -2,11 +2,16 @@ from pytximport import tximport
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.default_inference import DefaultInference
 from pydeseq2.ds import DeseqStats
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 inference = DefaultInference(n_cpus=8)
-t2g_full = pd.read_csv("/data1/yashjonjale/igem_stuff/ehux_study/sra_data/working_folder/w_datasets2/t2g.txt", sep="\t",
-                       names=["txid", "gene_id", "gene_name", "chrom", "start", "end", "strand"])
-t2g = t2g_full[["txid", "gene_id"]]
+t2g_full = pd.read_csv("/data1/yashjonjale/t2g.txt", sep="\t",
+                       names=["transcript_id", "gene_id", "gene_name", "transcript_name", "chrom", "start", "end", "strand"])
+t2g = t2g_full[["transcript_id", "gene_id"]]
+
+
 
 # Load metadata
 metadata = pd.read_csv("/home/schoudhary/github/Ehuxleyi/SRP182772/SRP182772.tsv", sep="\t")
@@ -15,8 +20,21 @@ metadata = metadata.sort_values("library_name")
 metadata["library_name"] = pd.Categorical(metadata["library_name"], categories=metadata["library_name"].unique())
 
 files = [f"/home/schoudhary/github/Ehuxleyi/SRP182772/snakemake_out/counts/{acc}/abundance.tsv" for acc in metadata.index]
-txi = pytximport.import_kallisto(files=files, tx2gene=t2g)
-counts = txi["counts"].round(0)
+#target_id       length  eff_length      est_counts      tpm
+result = tximport(file_paths=files, data_type='kallisto', transcript_gene_map=t2g,
+               id_column = "target_id",
+               counts_column = "est_counts",
+               length_column = "eff_length", abundance_column = "tpm")
+
+result.X = result.X.round().astype(int)
+#result = result[:, result.X.max(axis=0) > 10].copy()
+
+metadata["type"] = metadata["library_name"].str.split("-", n=1).str[0]
+metadata["type"] = pd.Categorical(metadata["type"], categories=metadata["type"].unique())
+metadata["replicate"] = metadata["library_name"].str.split("-", n=1).str[1]
+
+result.obs["type"] = metadata["type"]
+result.obs["replicate"] = metadata["replicate"]
 
 import pyranges as pr
 gtf = pr.read_gtf("/data1/yashjonjale/igem_stuff/ehux_study/sra_data/working_folder/kb_ref_out/Emiliania_huxleyi.Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0.59.gtf")
@@ -24,26 +42,29 @@ gtf_gene = gtf.df[gtf.df["Feature"] == "gene"][["gene_id", "gene_name"]].drop_du
 gtf_gene["gene_name"].fillna(gtf_gene["gene_id"], inplace=True)
 gtf_gene.set_index("gene_id", inplace=True)
 
-common_genes = counts.index.intersection(gtf_gene.index)
-counts = counts.loc[common_genes]
-counts.index = pd.Index(pd.Series(common_genes).apply(lambda x: pd.unique(gtf_gene.loc[x, "gene_name"])))
 
-metadata = metadata.loc[counts.columns]
-metadata["type"] = metadata["library_name"].str.split("-", n=1).str[0]
-metadata["type"] = pd.Categorical(metadata["type"], categories=metadata["type"].unique())
-metadata["replicate"] = metadata["library_name"].str.split("-", n=1).str[1]
+common_genes = result.var.index.intersection(gtf_gene.index)
 
-dds = DeseqDataSet(counts=counts, clinical=metadata, design_factors="type")
-dds.fit()
+result = result[:, result.var.index.isin(common_genes)]
+result.obs = metadata
+#.loc[result.obs.index]
 
-vsd = VST(dds)
-vsd_data = vsd.transform()
+dds = DeseqDataSet(adata=result, design_factors="type",     refit_cooks=True,
+                       inference=DefaultInference(n_cpus=8),
+                       quiet=True,
+                   )
+dds.deseq2()
 
-rld = RLOG(dds)
-rld_data = rld.transform()
 
-pca_vsd = vsd.plot_pca(group="type")
-pca_replicate = vsd.plot_pca(group="replicate")
+#vsd = vst(dds)
+#vsd_data = vsd.transform()
+dds.vst(use_design=True)
+
+from sklearn.decomposition import PCA
+df = dds.layers["vst_counts"]
+pca_vsd =  PCA(n_components=2)
+pca_vsd.fit(df)
+pca_vsd = pca_vsd.transform(df)
 
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
@@ -51,10 +72,10 @@ sns.scatterplot(x=pca_vsd[:, 0], y=pca_vsd[:, 1], hue=metadata["type"])
 plt.title("PCA (VST) by Type")
 
 plt.subplot(1, 2, 2)
-sns.scatterplot(x=pca_replicate[:, 0], y=pca_replicate[:, 1], hue=metadata["replicate"])
+sns.scatterplot(x=pca_vsd[:, 0], y=pca_vsd[:, 1], hue=metadata["replicate"])
 plt.title("PCA (VST) by Replicate")
 
 plt.tight_layout()
 plt.show()
 
-
+plt.savefig("test.pdf")
