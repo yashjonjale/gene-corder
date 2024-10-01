73bbcb923fb78c3a40cf66e8af94ecefe4681bd6
Yash Jonjale <yjonjale@gmail.com>
2024-10-01 21:55:37 +0530

    final commit

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 wget -c "https://raw.githubusercontent.com/yashjonjale/gene-corder/refs/heads/main/environment.yml"
-# conda create 
-# conda activate gc 
+eval "$(conda shell.bash hook)"
+conda deactivate
+conda env create -f environment.yml
+echo "GeneCorder environment created successfully"
+conda activate gc
+echo "Installing GeneCorder"
 pip install git+https://github.com/yashjonjale/gene-corder
+echo "To work with the tool activate the environment, run 'conda activate gc'"
