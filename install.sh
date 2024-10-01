93e55146b3b11f08998c625542f188db844c2d37
Saket Choudhary <saketkc@gmail.com>
2024-10-01 18:53:15 +0530

    Add install script

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
+wget -c "https://raw.githubusercontent.com/yashjonjale/gene-corder/refs/heads/main/environment.yml"
+# conda create 
+# conda activate gc 
 pip install git+https://github.com/yashjonjale/gene-corder
