67fc3060c3d00a55cbd997f1db8a0b9300f72b72
Saket Choudhary <saketkc@gmail.com>
2024-10-01 23:07:41 +0530

    More fixes to env.yml

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -eox pipefail
 wget -c "https://raw.githubusercontent.com/yashjonjale/gene-corder/refs/heads/main/env.yml"
 eval "$(conda shell.bash hook)"
 conda deactivate
+conda env remove -y -n genecorder || true
 conda env create -f env.yml
 echo "GeneCorder environment created successfully"
 conda activate genecorder
