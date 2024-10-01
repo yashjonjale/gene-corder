bd254e52a45a519b9b9dbc5ed4814958e6111d4d
Saket Choudhary <saketkc@gmail.com>
2024-10-01 23:00:52 +0530

    Update install

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+set -eox pipefail
 wget -c "https://raw.githubusercontent.com/yashjonjale/gene-corder/refs/heads/main/env.yml"
 eval "$(conda shell.bash hook)"
 conda deactivate
 conda env create -f env.yml
 echo "GeneCorder environment created successfully"
-conda activate gc1
+conda activate genecorder
 echo "Installing GeneCorder"
 pip install git+https://github.com/yashjonjale/gene-corder
-echo "To work with the tool activate the environment, run 'conda activate gc'"
+echo "To work with the tool activate the environment, run 'conda activate genecorder'"
